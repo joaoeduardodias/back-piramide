bdd12c6b14261013e12451700e76eddb5a432db3
João Dias <joaoeduardodias123@gmail.com>
2025-12-19 20:30:29 -0300

    fix: remove route test

--- src/http/routes/emails/index.ts
+++ src/http/routes/emails/index.ts
@@ -1,8 +1,6 @@
 import type { FastifyInstance } from 'fastify'
 import { sendOrderCreatedEmail } from './send-order-created-email'
-import { sendTestEmail } from './send-test-email'
 
 export async function emailRoutes(app: FastifyInstance) {
-  await sendTestEmail(app)
   await sendOrderCreatedEmail(app)
 }
